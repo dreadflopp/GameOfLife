91ef3a8ca116dbc651f8729dd5174c88b5bfab28
Andreas Lind <anli1606@student.miun.se>
2017-10-25 15:33:16 +0200

    Created documentation for Population.H

--- include/Cell_Culture/Population.h
+++ include/Cell_Culture/Population.h
@@ -17,17 +17,16 @@
 
 using namespace std;
 
-/*
-Representation of the complete society of cell existance and interactions.
-
-The Population constitutes of all current, previous and future generations of cells, both living and dead
-as well as those not yet born. By mapping all cells to their respective positions in the simulation world,
-Population has the complete knowledge of each cell's whereabouts. Furthermore, the class is responsible for
-determining which rules should be required from the RuleFactory, and store the pointer to these as members.
-
-Population's main responsibility during execution is determining which rule to apply for each new generation
-and updating the cells to their new states.
-*/
+/**
+ * @class Population
+ * @brief Representation of the complete society of cell existance and interactions
+ * @details The Population constitutes of all current, previous and future generations of cells, both living and dead
+ * as well as those not yet born. By mapping all cells to their respective positions in the simulation world,
+ * Population has the complete knowledge of each cell's whereabouts. Furthermore, the class is responsible for
+ * determining which rules should be required from the RuleFactory, and store the pointer to these as members.
+ * Population's main responsibility during execution is determining which rule to apply for each new generation
+ * and updating the cells to their new states.
+ */
 class Population
 {
 private:
@@ -40,16 +39,47 @@ private:
     void buildCellCultureFromFile();
 
 public:
+    /**
+     *  @brief Constructor that initializes empty Population object
+     *  @details Initializes Population object with generation = 0 and evenRuleExistance and oddRuleExistance as nullptrs
+     */
     Population() : generation(0), evenRuleOfExistence(nullptr), oddRuleOfExistence(nullptr) {}
+
+    /**
+    *   @brief Destructor för deallocating Population object
+    *   @details Deallocates evenRuleExistance and oddRuleExistance if not set as nullptrs
+    */
     ~Population();
 
+    /**
+     *  @brief Function to initialize Population object with cells and evenRuleName and oddRuleName
+     *  @param evenRuleName represents rules to be applied to every even numbered generation of cells
+     *  @param oddRuleName represents rules to be applied to every odd numbered generation of cells. If left empty,
+     *  same rule as evenRuleName will be applied
+     *  @test that nr of cells in the simulation world is equal to height * width
+     */
     void initiatePopulation(string evenRuleName, string oddRuleName = "");
+
+    /**
+     *  @brief Function updates cells next state and applies rules for next generation
+     *  @return Nr of generations incremented by 1
+     *  @test require generation to be incremented by 1
+     */
     int calculateNewGeneration();
 
-    // Returns cell by specified key value.
+    /**
+     *  @brief Returns reference to cell specified by position
+     *  @param position Represents coordinates in map with cells
+     *  @return Cell at specified position
+     */
     Cell& getCellAtPosition(Point position) { return cells.at(position); }
 
-    // Gets the total amount of cells in the population, regardless of state.
+    /**
+     *  @brief Returns total number of cells in population
+     *  @details Returns cells.size() which equals the number of height * width of simulation world
+     *  @return cells.size() Represents size of Map of cells
+     *  @test after Population object first has been created, require return value == 0
+     */
     int getTotalCellPopulation() { return cells.size(); }
 
 };

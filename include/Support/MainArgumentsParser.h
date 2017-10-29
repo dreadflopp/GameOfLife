ae5a8438a5b4807891ae6eeadc4b4e134b6a3473
Andreas Lind <anli1606@student.miun.se>
2017-10-29 10:01:06 +0100

    added tests and removed documentation for private functions

--- include/Support/MainArgumentsParser.h
+++ include/Support/MainArgumentsParser.h
@@ -29,28 +29,16 @@ public:
      * @param length represents nr of arguments
      * @return reference to ApplicationValues object
      * @test that appValues are set as expected
+     * @test that if no even rule name has been set, default to conway
+     * @test that if no odd rule name has been set, default to same as even rule name
      */
     ApplicationValues& runParser(char* argv[], int length);
 
 private:
     ApplicationValues appValues;
 
-    /**
-     * @brief Checks if a given option exists
-     * @param begin represents starting point of list of provided arguments
-     * @param end represents end point of list of provided arguments
-     * @param option represents value to checked if it exists in provided arguments
-     * @return true if parameter 'option' exists in provided arguments
-     */
     bool optionExists(char** begin, char** end, const std::string& option);
 
-    /**
-     * @brief gets the given option value
-     * @param begin represents starting point of list of provided arguments
-     * @param end represents end point of list of provided arguments
-     * @param option represents value to find in provided arguments
-     * @return pointer to value to be set in appValues
-     */
     char* getOption(char** begin, char** end, const std::string& option);
 };
 

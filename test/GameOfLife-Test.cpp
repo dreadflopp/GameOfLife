f2e6d239fa5c1159f9de03413ac029edef3b8893
Mattias Lindell <mali1624@student.miun.se>
2017-10-29 16:38:36 +0100

    Test preparations Edited CMakeList.txt and added test build Added main-test.cpp and GameOfLife-Test.cpp. GameOfLife-Test.cpp is where all test code will be placed

---
+++ test/GameOfLife-Test.cpp
@@ -0,0 +1,9 @@
+/*
+ * file GameOfLife-Test.cpp
+ * author Mattias Lindell
+ * author Andreas Lind
+ * date 29 october 2017
+ * version 0.1
+*/
+#include "catch.hpp"
+#include "memstat.hpp"
---
+++ test/main-test.cpp
@@ -0,0 +1,9 @@
+/*
+ * file main-test.cpp
+ * author Mattias Lindell
+ * author Andreas Lind
+ * date 29 october 2017
+ * version 0.1
+*/
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"

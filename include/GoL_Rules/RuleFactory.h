764328437b439a3e5b97581fca42a770affbfe34
Andreas Lind <anli1606@student.miun.se>
2017-10-29 12:03:24 +0100

    added documentation to RuleFactory.h

--- include/GoL_Rules/RuleFactory.h
+++ include/GoL_Rules/RuleFactory.h
@@ -1,8 +1,9 @@
-/*
- * Filename    RuleFactory.h
- * Author      Erik Ström
- * Date        October 2017
- * Version     0.1
+/**
+ * @file RuleFactory.h
+ * @author Mattias Lindell
+ * @author Andreas Lind
+ * @date 2017-10-29
+ * @version 0.1
 */
 
 #ifndef RULEFACTORY_H
@@ -10,14 +11,31 @@
 
 #include "GoL_Rules/RuleOfExistence.h"
 
-// Singleton class to handle creation of RulesOfExistence objects.
+/**
+ * @class RuleFactory singleton class to handle creation of RulesOfExistence objects
+ * @brief singleton class makes sure only one RuleFactory object is created which then can create and
+ * return specified RuleOfExistance objects
+ * @test that only one instance of class can be created
+ */
 class RuleFactory
 {
 private:
     RuleFactory() {}
 
 public:
+    /**
+     * @brief function that creates RuleFactory object and returns reference to that object
+     * @return reference to RuleFactory object
+     */
     static RuleFactory& getInstance();
+    /**
+     * @brief function creates RuleOfExistance objects
+     * @details function creates RuleOfExistance object(rules) based on param ruleName
+     * @param cells passed by reference and represents cells in population which rules should be applied to
+     * @param ruleName represents rule to be applied. Default is 'conway'
+     * @return pointer to RuleOfExistance object
+     * @test that new rule has been created as expected
+     */
     RuleOfExistence* createAndReturnRule(map<Point, Cell>& cells, string ruleName = "conway");
 };
 

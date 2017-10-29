ec5b18a9aca64e5a4e774a19bc5848b66a65da98
Andreas Lind <anli1606@student.miun.se>
2017-10-29 20:08:28 +0100

    added documentation to RuleOfExistance_VonNeumann.h

--- include/GoL_Rules/RuleOfExistence_VonNeumann.h
+++ include/GoL_Rules/RuleOfExistence_VonNeumann.h
@@ -1,20 +1,22 @@
-/*
- * Filename    RuleOfExistence_VonNeumann.h
- * Author      Erik Ström
- * Date        October 2017
- * Version     0.1
+/**
+ * @file RuleOfExistence_VonNeumann.h
+ * @author Mattias Lindell
+ * @author Andreas Lind
+ * @date 2017-10-29
+ * @version 0.1
 */
 
 #ifndef GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
 #define GAMEOFLIFE_RULEOFEXISTENCE_VONNEUMANN_H
 
 #include "RuleOfExistence.h"
-/*
-Von Neumann's RuleOfExistence, differs from Conway in that only 4 neighbours are accounted for.
 
-Concrete Rule of existence, implementing Von Neumann's rule.
-Only difference from Conway is that neighbours are determined using only cardinal directions (N, E, S, W).
-*/
+/**
+ * @class RuleOfExistence_VonNeumann
+ * @brief Von Neumann's RuleOfExistence, differs from Conway in that only 4 neighbours are accounted for.
+ * @details Concrete Rule of existence, implementing Von Neumann's rule.
+ * Only difference from Conway is that neighbours are determined using only cardinal directions (N, E, S, W).
+ */
 class RuleOfExistence_VonNeumann : public RuleOfExistence
 {
 private:
@@ -24,6 +26,10 @@ public:
             : RuleOfExistence({ 2,3,3 }, cells, CARDINAL, "von_neumann") {}
     ~RuleOfExistence_VonNeumann() {}
 
+    /**
+     * @brief Execute the rule specific for VonNeumann
+     * @test that only cell neighbours in CARDINAL directions are counted
+     */
     void executeRule();
 };
 
